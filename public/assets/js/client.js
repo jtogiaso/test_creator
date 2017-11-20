b71413f85e32b65bbea05b26f2a3ed3ffcaf8872
Dan Moneypenny <catullus1973@gmail.com>
2017-11-20 00:06:06 -0800

    Updated createT and client.js

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,1 +1,53 @@
 console.log("Client.js is loaded.");
+//////////////////////////////////////////////
+// Log in functions
+//TO DO validation
+
+var currentUser;
+
+var setUser = function() {
+	currentUser = $("input[name='user_name']").val().trim();
+	sessionStorage.setItem("username", currentUser);
+};
+
+var getUser = function() {
+	currentUser = sessionStorage.getItem("username");
+	if(currentUser) {
+		$("#user-label").text(currentUser);
+	} else {
+		$("#user-label").text("ERROR, missing username");
+	}
+};
+
+$("#login").on("click", function(event) {
+	event.preventDefault();
+	
+	setUser();
+	
+});
+
+
+
+
+$(document).ready(function() {
+	
+	getUser();
+
+
+});//end document ready
+
+
+
+
+
+
+/*
+
+
+$("").on("click", function(event) {
+	event.preventDefault();
+	
+});
+
+*/
+
