c5498bce1c061487e348b0c59abac478a51da05d
matthewnguyen <matthewnguyen19@gmail.com>
2017-11-20 18:12:52 -0800

    created 'create', 'update', and 'delete' functions in db_controller

--- app/controllers/db_controller.js
+++ app/controllers/db_controller.js
@@ -9,5 +9,107 @@ module.exports = {
 			}
 		)
 	},
+
+	create_question: (testId, questionPhrase) => {
+		return models.Question.create(
+			{
+				TestId: testId,
+				question_phrase: questionPhrase
+			}
+		)
+	},
+
+	create_answer: (questionId, answerPhrase, isCorrect) => {
+		return models.Answer.create(
+			{
+				QuestionId: questionId,
+				answer_phrase: answerPhrase,
+				correct_answer: isCorrect
+			}
+		)
+	},
+
+	update_test_name: (testId, testName) => {
+		return models.Test.update(
+			{
+				test_name: testName
+			},
+			{
+				where: 
+				{
+					TestId: testId
+				}
+			}
+		)
+	},
+
+	update_question: (questionId, questionPhrase) => {
+		return models.Question.update(
+			{
+				question_phrase: questionPhrase
+			},
+			{
+				where:
+				{
+					QuestionId: questionId
+				}
+			}
+		)
+	},
 	
+	update_answer: (answerId, answerPhrase) => {
+		return models.Answer.update(
+			{
+				answer_phrase: answerPhrase
+			},
+			{
+				where:
+				{
+					AnswerId: answerId
+				}
+			}
+		)
+	},
+
+	delete_test: (testId) => {
+		return models.Test.update(
+		{
+			deleted: true
+		},
+		{
+			where:
+			{
+				TestId: testId
+			}
+		}
+		)
+	},
+
+	delete_question: (questionId) => {
+		return models.Question.update(
+		{
+			deleted: true
+		},
+		{
+			where:
+			{
+				QuestionId: questionId
+			}
+		}
+		)
+	},
+
+	delete_answer: (answerId) => {
+		return models.Answer.update(
+		{
+			deleted: true
+		},
+		{
+			where:
+			{
+				AnswerId: answerId
+			}
+		}
+		)
+	}
 };
