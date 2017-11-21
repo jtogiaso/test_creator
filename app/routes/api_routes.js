d0e06c7c49afbdd4a0c92c90d2e17044f2f142ab
Fiaalii Togiaso <jtogiaso@gmail.com>
2017-11-20 20:35:15 -0800

    Rearranging apit routes

--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -11,20 +11,43 @@ const api_controller = require("../controllers/api_controller.js");
 // =============================================================
 module.exports = function(app) {
 
-// Get all tests
+// ===============================================================================
+
+// Get all tests test creator
+  app.get("/api/tests", api_controller.get_all_test);
+
+// Get all questions and related `answers` for a test
+  app.get("/api/questions", api_controller.get_all_question);
+
+// // Get all tests
+//   app.get("/api/answers", api_controller.get_all_answer);
+
+// // Get one result
+//   app.get("/api/result", api_controller.get_result);  
+
+// Get all results by user 
+  app.get("/api/all_result_user", api_controller.get_all_result_user);
+
+ // Get all results by test -> for test takers only
+  app.get("/api/all_result_test", api_controller.get_all_result_test);
+
+
+// Get one test for test taker, should also retrive all the questions and answers
   app.get("/api/test", api_controller.get_test);
 
-// Get all tests
-  app.get("/api/question", api_controller.get_question);
+  // ===============================================================================
 
-// Get all tests
-  app.get("/api/answer", api_controller.get_answer);
 
 // Add a new test
   app.post("/api/test", api_controller.create_test);
 
 // Add a question to test
   app.post("/api/question", api_controller.create_question);
+
+//Add a result
+  app.post("/api/result", api_controller.create_result)
+
+  // ===============================================================================
 
 //Update a question
   app.put("/api/question", api_controller.update_question);
@@ -34,6 +57,11 @@ module.exports = function(app) {
 
 //Update an answer
   app.put("/api/answer", api_controller.update_answer);
+
+//Update a result
+  app.put("/api/answer", api_controller.update_result);
+
+// ===============================================================================
 
 //Delete user
   app.delete("/api/user", api_controller.delete_user);
@@ -47,40 +75,3 @@ module.exports = function(app) {
 //Delete Answer
   app.delete("/api/answer", api_controller.delete_answer);
 
-
-
-
-
-
-
-  // app.get("/api/:test_code" , api_controller.get_test);
-
-  //Ajax call route
-  // db.Question.findAll(
-  //   {
-  //     where: {
-  //       TestId: req.body.TestId
-  //     }
-  //   }
-  // )
-  // .then(function(dbQuestions){
-  //   let testOBJ = {}
-  //   for (let i in dbQuestions){
-  //     testObj[i].question=dbQuestions[i].question_phrase;
-  //     db.Anwers.findAll(
-  //       {
-  //         where: {
-  //           QuestionId: dbQuestions.question_id
-  //         }
-  //       }
-  //     )
-  //     .then(function(dbAnswers){
-  //       for (let j in dbAnswers)
-  //         testOBJ[i].answer[j]=dbAnswers.answer_phrase;
-  //     })
-  //   }
-  // })
-
-  // app.get("api/all_questions_and_answers", api_controller.
-  // });
-}; 
